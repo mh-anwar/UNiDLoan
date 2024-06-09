d208ce4a30608299ca3a56293a633821f0a9a285
mh-anwar <mohammad.anwar2939@gmail.com>
2024-06-09 03:40:20 -0400

    Add video's endpoint

--- app/api/student/route.js
+++ app/api/student/route.js
@@ -1,14 +1,31 @@
 // General route to get data of all students
+import { NextResponse } from 'next/server';
 import Student from './StudentSchema';
 import mongoose from 'mongoose';
 
 export async function GET(req) {
-	// Get all students
-	try {
-		await mongoose.connect(process.env.MONGODB);
-		const students = await Student.find();
-		return new Response(JSON.stringify(students), { status: 200 });
-	} catch (err) {
-		return new Response(JSON.stringify(err), { status: 500 });
-	}
+    const query = new URL(req.url);
+    const resourceType = query.searchParams.get('resourceType');
+    // by default the resourceType is set to student
+    const filter = resourceType ? resourceType : 'student';
+    console.log(filter);
+    // Get all students
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        const students = await Student.find();
+
+        if (filter === 'student') {
+            return NextResponse.json(JSON.stringify(students), { status: 200 });
+        } else if (filter === 'videos') {
+            // Get all video fields from every student and return
+            const allVideos = students
+                .map((student) => student.video)
+                .filter((video) => video != null);
+            return NextResponse.json(JSON.stringify(allVideos), {
+                status: 200,
+            });
+        }
+    } catch (err) {
+        return new Response(JSON.stringify(err), { status: 500 });
+    }
 }
