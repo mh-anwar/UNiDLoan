305dc18e16ed8cf1cee2eb192609477c033207e0
mh-anwar <mohammad.anwar2939@gmail.com>
2024-06-09 04:57:51 -0400

    Create login with backend

--- app/layout.js
+++ app/layout.js
@@ -1,45 +1,52 @@
 'use client';
-import "@/app/globals.css";
-import { Navigation } from "./components/Navigation";
-import React from "react";
-import { AnimatePresence } from "framer-motion";
-import ContractOverlay from "./components/contract";
-import { useState } from "react";
-import CreateContractOverlay from "./components/createContract";
-import Login from "./components/Login";
+import '@/app/globals.css';
+import { Navigation } from './components/Navigation';
+import React from 'react';
+import { AnimatePresence } from 'framer-motion';
+import ContractOverlay from './components/contract';
+import { useState } from 'react';
+import CreateContractOverlay from './components/createContract';
+import Login from './components/Login';
 
 // context
 export const UserContext = React.createContext(null);
 
 // Layout Component
-export default function RootLayout({ children })
-{
-  const [displayCreateContract, setDisplayCreateContract ] = useState(false);
-  const [displayContract, setDisplayContract] = useState(false);
-  const [displayLogin, setDisplayLogin] = useState(false);
-  const [username, setUsername] = useState("");
+export default function RootLayout({ children }) {
+    const [displayCreateContract, setDisplayCreateContract] = useState(false);
+    const [displayContract, setDisplayContract] = useState(false);
+    const [displayLogin, setDisplayLogin] = useState(false);
+    const [nearID, setNearID] = useState('');
 
-  return (
-    <UserContext.Provider value={{ displayContract: displayContract, setDisplayContract: setDisplayContract, username: username, setUsername: setUsername, displayLogin: displayLogin, setDisplayLogin: setDisplayLogin }}>
-      <html lang="en" className="dark">
-        <body className="flex flex-col">
-          <div className="w-full h-screen">
-            <Navigation />
-            <br/>
-            {children}
-            <AnimatePresence
-              initial={false}
-              mode="wait"
-              onExitComplete={() => null}
-            >
-              {displayCreateContract && <CreateContractOverlay />}
-              {displayContract && <ContractOverlay/>}
-              {displayLogin && <Login/>}
-            </AnimatePresence>
-          </div>
-
-        </body>
-      </html>
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider
+            value={{
+                displayContract: displayContract,
+                setDisplayContract: setDisplayContract,
+                nearID: nearID,
+                setNearID: setNearID,
+                displayLogin: displayLogin,
+                setDisplayLogin: setDisplayLogin,
+            }}
+        >
+            <html lang="en" className="dark">
+                <body className="flex flex-col">
+                    <div className="w-full h-screen">
+                        <Navigation />
+                        <br />
+                        {children}
+                        <AnimatePresence
+                            initial={false}
+                            mode="wait"
+                            onExitComplete={() => null}
+                        >
+                            {displayCreateContract && <CreateContractOverlay />}
+                            {displayContract && <ContractOverlay />}
+                            {displayLogin && <Login />}
+                        </AnimatePresence>
+                    </div>
+                </body>
+            </html>
+        </UserContext.Provider>
+    );
 }
