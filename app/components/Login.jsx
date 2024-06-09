15a469e853d8840a6eab53742f16e4a279dca4f2
FrostDev <frostdev123friday@gmail.com>
2024-06-08 23:41:48 -0400

    fixed modals

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,9 +1,8 @@
-"use client";
-import { useState, useContext, CSSProperties } from "react"
-import { UserContext } from "../layout";
-import Backdrop from "../common/backdrop";
-import { motion, AnimatePresence } from "framer-motion";
-import BouncingDotsLoader from "../common/loader/loader";
+import { useState, useContext, CSSProperties } from 'react';
+import { UserContext } from '../layout';
+import Backdrop from '../common/backdrop';
+import { motion, AnimatePresence } from 'framer-motion';
+import BouncingDotsLoader from '../common/loader/loader';
 
 const loginAnim = {
     hidden: {
@@ -28,19 +27,11 @@ const loginAnim = {
     },
 }
 
-const contract = 
-{
-    amount: 1000.0,
-    interest: 0.1,
-    date: Date.now(),
-}
+export default function Login() {
+    const { displayLogin, setDisplayLogin } = useContext(UserContext);
+    const { username, setUsername } = useContext(UserContext);
 
-export default function ContractOverlay() {
-
-    const { displayContract, setDisplayContract } = useContext(UserContext);
-    const { user, setUser } = useContext(UserContext);
-
-    const [username, setUsername] = useState('')
+    const [username_, setUsername_] = useState('')
     const [password, setPassword] = useState('')
 
     const [userState, setUserState] = useState({ state: 'empty', msg: '' })
@@ -55,9 +46,9 @@ export default function ContractOverlay() {
             initial="hidden"
             animate="visible"
             exit="exit"
-            className="font-sgt w-[30rem] bg-black/95 rounded-2xl border-2 border-zinc-900/50 p-10 flex flex-col gap-5 pointer-events-auto overflow-clip"
+            className="w-[30rem] bg-black/95 rounded-2xl border-2 border-zinc-900/50 p-10 flex flex-col gap-5 pointer-events-auto overflow-clip"
         >
-            <h1 className="text-4xl font-bold text-gray-300">Contracts</h1>
+            <h1 className="font-sg text-4xl font-bold text-gray-300">Get Started!</h1>
             <p className="text-gray-400">An account will allow you to save your progress, participate in contests, and more!</p>
 
             <div>
@@ -79,24 +70,6 @@ export default function ContractOverlay() {
             </div>
 
             <div className="flex flex-row w-full gap-5">
-                <button
-                    className="bg-blue-950 hover:bg-slate-950/50 duration-200 text-gray-300 font-semibold py-3 px-4 border-2 border-blue-800/20 rounded shadow inline-flex items-center grow"
-                    onClick={() => {
-                        setLoading(true);
-                        console.log("start loading");
-
-                        AuthService.SignUp({ username, password }, setUserState, setPassState, setUser, setAuthOverlay)
-                        .then(() => {
-                            console.log("end loading");
-                            setLoading(false)
-                        });
-                    }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
-                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        <path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z" />
-                    </svg>
-                    <span className="text-center w-full">Sign Up</span>
-                </button>
 
                 <button
                     className="bg-blue-950 hover:bg-slate-950/50 duration-200 text-gray-300 font-semibold py-3 px-5 border-2 border-blue-800/20 rounded shadow inline-flex items-center grow"
@@ -114,7 +87,7 @@ export default function ContractOverlay() {
                         <path fillRule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
                         <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
                     </svg>
-                    <span className="text-center w-full">Log In</span>
+                    <span className="text-center w-full">Join</span>
                 </button>
             </div>
 
@@ -139,10 +112,9 @@ export default function ContractOverlay() {
     return (
         <Backdrop
             onClick={() => {
-                console.log("???")
                 if(!loading)
                 {
-                    setDisplayContract(false)
+                    setDisplayLogin(false)
                 }
             }}
             children={modal}>
--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -2,14 +2,25 @@
 import { useEffect, useState } from 'react';
 
 import InvestMoney from './InvestMoney';
+import { UserContext } from '../layout';
+import { useContext } from 'react';
 
-export function Navigation() {
+export function Navigation()
+{
 	const [label, setLabel] = useState('Login');
+    const { displayLogin, setDisplayLogin } = useContext(UserContext);
 
 	return (
-		<nav className='flex flex-row justify-end w-full p-2'>
+		<nav className='flex flex-row justify-end w-full p-2 gap-2'>
 			<InvestMoney />
-			<button className='bg-blue-500'> {label} </button>
+			<button
+				className='bg-blue-500'
+				onClick={() => {
+					setDisplayLogin(true)
+				}}
+			>
+				{label}
+			</button>
 		</nav>
 	);
 }
