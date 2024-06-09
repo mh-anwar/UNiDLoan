932c04fc02eea4a6e4e1bbbdb9dc8af7eb4e3f75
mh-anwar <mohammad.anwar2939@gmail.com>
2024-06-09 03:59:23 -0400

    Unabstract videos and branch into dir

--- app/api/student/videos/route.js
+++ app/api/student/videos/route.js
@@ -1,15 +1,19 @@
 // General route to get data of all students
 import { NextResponse } from 'next/server';
-import Student from './StudentSchema';
 import mongoose from 'mongoose';
+import Student from '../StudentSchema';
 
 export async function GET(req) {
-    // Get all students
     try {
         await mongoose.connect(process.env.MONGODB);
         const students = await Student.find();
-
-        return NextResponse.json(JSON.stringify(students), { status: 200 });
+        // Get all video fields from every student and return
+        const allVideos = students
+            .map((student) => student.video)
+            .filter((video) => video != null);
+        return NextResponse.json(JSON.stringify(allVideos), {
+            status: 200,
+        });
     } catch (err) {
         return new Response(JSON.stringify(err), { status: 500 });
     }
